6e37326c9b843305ef796f7f3c82d6b785aa9868
snorri-agust-snorrason <vaskur_@hotmail.com>
2015-03-09 20:55:11 +0000

    grp
    bla

---
+++ Pix/app/src/main/java/com/development/napptime/pix/CreateGroupActivity.java
@@ -0,0 +1,93 @@
+package com.development.napptime.pix;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+
+public class CreateGroupActivity extends Activity {
+
+    public boolean waiting;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_group);
+        waiting = false;
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_signup, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void signUp(View view)
+    {
+        if(waiting ==true)
+            return;
+
+
+        String name = ((EditText) findViewById(R.id.editTextName)).getText().toString();
+        String email = ((EditText) findViewById(R.id.editTextEmail)).getText().toString();
+        String password = ((EditText) findViewById(R.id.editTextPassword)).getText().toString();
+        String passwordConfirm = ((EditText) findViewById(R.id.editTextPasswordConfirm)).getText().toString();
+
+        if(!password.equals(passwordConfirm))
+        {
+            Toast toast = Toast.makeText(getApplicationContext(), "Password's don't match.", Toast.LENGTH_SHORT);
+            toast.show();
+            return;
+        }
+
+        ParseUser user = new ParseUser();
+        user.setUsername(name);
+        user.setPassword(password);
+        user.setEmail(email);
+
+
+// other fields can be set just like with ParseObject
+        waiting = true;
+        user.signUpInBackground(new SignUpCallback() {
+            public void done(ParseException e) {
+                if (e == null) {
+                    waiting = false;
+                    Toast toast = Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_SHORT);
+                    toast.show();
+                    // Hooray! Let them use the app now.
+                } else {
+                    waiting = false;
+                    Toast toast = Toast.makeText(getApplicationContext(), "nooSuccess", Toast.LENGTH_SHORT);
+                    toast.show();
+                    // Sign up didn't succeed. Look at the ParseException
+                    // to figure out what went wrong
+                }
+            }
+        });
+    }
+}

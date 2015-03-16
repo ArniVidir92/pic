2c2012a7e06e002e480d8f7ba7fbb23fd5821a30
Ívan Bjarni Jónsson <ibj9@hi.is>
2015-03-16 16:58:34 +0000

    Added onChangeListener to the rating bar. Also some minor refactoring. Removed the header and footer at login to improve performance.

--- Pix/app/src/main/java/com/development/napptime/pix/Utility.java
+++ Pix/app/src/main/java/com/development/napptime/pix/Utility.java
@@ -6,14 +6,31 @@ import java.util.List;
  * Created by Napptime on 3/16/15.
  */
 public class Utility {
-    public static double calculateAverage(List<Integer> marks) {
+    public static double calculateAverage(List<Integer> ratings) {
         Integer sum = 0;
-        if(!marks.isEmpty()) {
-            for (Integer mark : marks) {
-                sum += mark;
+        if(!ratings.isEmpty()) {
+            for (Integer rating : ratings) {
+                sum += rating;
             }
-            return sum.doubleValue() / marks.size();
+            return sum.doubleValue() / ratings.size();
         }
         return sum;
+    }
+
+    public static int indexOfIn(String theSearched, String theSearchable) {
+        theSearched = theSearched.toLowerCase();
+        theSearchable = theSearchable.toLowerCase();
+        String[] array = theSearchable.split("|");
+
+        for (int i = 0; i < array.length; i++) {
+            if(theSearched.equals(array[i]));
+        }
+
+        return -1;
+    }
+
+    public static String addToStringList(String list, String added) {
+
+        return list+"|"+added;
     }
 }
